eb6a6c2e9f971b675edc3b4a244d120fe548e3b5
Quinn Bancescu <quinn.bancescu@gmail.com>
2023-12-03 13:37:36 -0600

    don't turn in on yourself

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,28 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { Board } from './models/board.model';
 import { Snake } from './models/snake.model';
-import { Direction, Move } from './models/move.model';
+import { Direction, Move, WeightedMoves } from './models/move.model';
 import { Turn } from './models/turn.model';
 
 @Injectable()
 export class AppService {
+  private lastMove: Direction = null;
   getHello(): string {
     return 'Hello World!';
   }
 
   basicStragey(turn: Turn): Move {
+    const weightedMoves = new WeightedMoves();
+
     return new Move(Direction.UP, 'Matt sucks');
   }
 
-  findFood(board: Board, you: Snake): Direction {
+  private findFood(board: Board, you: Snake, range: number): Direction {
     return Direction.UP;
   }
 
-  dontKillYourSelf(board: Board, you: Snake): Direction {
-    return Direction.UP;
+  private dontKillYourSelf(you: Snake, weightedMoves: WeightedMoves): void {
+    if(this.lastMove === null){
+      return;
+    }
+    switch(this.lastMove) {
+      case Direction.DOWN:
+        weightedMoves.setWeight(Direction.UP, -10);
+        break;
+      case Direction.UP:
+        weightedMoves.setWeight(Direction.DOWN, -10);
+        break;
+      case Direction.LEFT:
+        weightedMoves.setWeight(Direction.RIGHT, -10);
+        break;
+      case Direction.RIGHT:
+        weightedMoves.setWeight(Direction.LEFT, -10);
+        break;
+    }
   }
 
-  attackOtherSnakes(board: Board, you: Snake): Direction {
+  private attackOtherSnakes(board: Board, you: Snake): Direction {
     return Direction.UP;
   }
 }
--- src/models/move.model.ts
+++ src/models/move.model.ts
@@ -13,3 +13,29 @@ export enum Direction {
   UP = 'UP',
   DOWN = 'DOWN',
 }
+
+export class WeightedMoves {
+  moves: WeightedMove[];
+
+  constructor() {
+    this.moves = [];
+    this.moves.push(new WeightedMove(Direction.UP, 0));
+    this.moves.push(new WeightedMove(Direction.DOWN, 0));
+    this.moves.push(new WeightedMove(Direction.LEFT, 0));
+    this.moves.push(new WeightedMove(Direction.RIGHT, 0));
+  }
+
+  setWeight(direction: Direction, weight: number) {
+    const index = this.moves.findIndex((move) => move.direction === direction);
+    this.moves[index].weight = weight;
+  }
+}
+
+class WeightedMove {
+  direction: Direction;
+  weight: number;
+  constructor(d: Direction, w: number) {
+    this.direction = d;
+    this.weight = w;
+  }
+}
